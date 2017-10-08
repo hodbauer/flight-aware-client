570f2f2068ea0e3cacc605173fd88525b64c4281
hodbauer <hodbod@gmail.com>
2017-10-08 11:56:44 +0300

    add and show cesium

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CesiumContainerComponent } from './components/cesium-container/cesium-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CesiumContainerComponent
   ],
   imports: [
     BrowserModule
